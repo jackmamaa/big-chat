f9fcd3fab211311ff68fef5372dc39cda5d2deac
jackmamaa <119478410+jackmamaa@users.noreply.github.com>
2023-03-03 13:51:19 +0800

    Update _chat.js

--- js/_chat.js
+++ js/_chat.js
@@ -33,11 +33,11 @@ window.onclick = function(event) {
 //Copy and edit data
 var _copy = new ClipboardJS('.copy_btn');
 _copy.on('success', function(e) {
-    layer.msg('复制成功',{time:800});
+    layer.msg('复制成功',{time:600});
     e.clearSelection();
 });
 _copy.on('error', function(e) {
-    layer.msg("复制失败",{time:1000});
+    layer.msg('复制成功',{time:1000});
 });
 
 $(document).on('click','#edit_data', function(e) {
@@ -50,7 +50,7 @@ $(document).on('click','#edit_data', function(e) {
         btn: ['提交','取消'],
         content: `
         <div style='display:flex;justify-content:center;'>
-            <textarea id='edit_box' oninput='auto_grow(this)' autosize>${text}</textarea>
+            <textarea id='edit_box' oninput='auto_grow(this)'>${text}</textarea>
         </div>`,
         yes:function (index,layero) {
             layer.closeAll();
@@ -70,10 +70,9 @@ $(document).on('click','#edit_data', function(e) {
         var areas = document.getElementById('edit_box');
         areas.focus();
         areas.setSelectionRange(areas.value.length,areas.value.length);
-        var keyEvent = new KeyboardEvent("keydown", {key : "a"});
-        document.dispatchEvent(keyEvent);
     },50);
 });
+
 //Style table switch
 $(document).on('click','#switch_btn', function(e) {
     var theme = document.getElementsByTagName('link')[1];
@@ -97,7 +96,6 @@ const msgerChat = get("#article-wrapper");
 const msgerSendBtn = get(".send-btn");
 const restart_chat = get("#restart_chat");
 const prompt_tips = get(".prompt_tips");
-// Icons made by Freepik from www.flaticon.com
 const BOT_IMG = "static/big_clever.svg";
 const PERSON_IMG = "static/human.svg";
 const BOT_NAME = "大聪明";
@@ -255,9 +253,9 @@ function appendMessage(name, img, side, text, id) {
     `;
     msgerChat.insertAdjacentHTML("beforeend", msgHTML);
     if (side == "left") {
-        $("."+holder).append(`<div title="点击复制" style="width:35px;margin:0 5 0 5;"><svg t="1676979873149" class="copy_btn" id="copy_data" data-clipboard-target=a${id} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6847" width="25" height="25"><path d="M576 384a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64H234.666667a64 64 0 0 1-64-64V448a64 64 0 0 1 64-64h341.333333z m0 64H234.666667v341.333333h341.333333V448z m-64 192v64H298.666667v-64h213.333333zM789.333333 170.666667a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64h-106.666666v-64h106.666666V234.666667H448v106.666666h-64v-106.666666a64 64 0 0 1 64-64h341.333333zM512 533.333333v64H298.666667v-64h213.333333z" fill="#707070" p-id="6848"></path></svg></div>`);
+        $("."+holder).append(`<div title="点击复制" style="width:35px;margin:0 5 0 5;"><svg t="1676979873149" class="copy_btn" id="copy_data" data-clipboard-target=#a${id} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6847" width="25" height="25"><path d="M576 384a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64H234.666667a64 64 0 0 1-64-64V448a64 64 0 0 1 64-64h341.333333z m0 64H234.666667v341.333333h341.333333V448z m-64 192v64H298.666667v-64h213.333333zM789.333333 170.666667a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64h-106.666666v-64h106.666666V234.666667H448v106.666666h-64v-106.666666a64 64 0 0 1 64-64h341.333333zM512 533.333333v64H298.666667v-64h213.333333z" fill="#707070" p-id="6848"></path></svg></div>`);
     } else {
-        $("."+holder).append(`<div title="编辑提问" style="width:35px;margin:0 5 0 5;"><svg t="1677572686028" class=a${id} id="edit_data" data-clipboard-target=#a${id} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2880" width="20" height="20"><path d="M862.709333 116.042667a32 32 0 1 1 45.248 45.248L455.445333 613.813333a32 32 0 1 1-45.258666-45.258666L862.709333 116.053333zM853.333333 448a32 32 0 0 1 64 0v352c0 64.8-52.533333 117.333333-117.333333 117.333333H224c-64.8 0-117.333333-52.533333-117.333333-117.333333V224c0-64.8 52.533333-117.333333 117.333333-117.333333h341.333333a32 32 0 0 1 0 64H224a53.333333 53.333333 0 0 0-53.333333 53.333333v576a53.333333 53.333333 0 0 0 53.333333 53.333333h576a53.333333 53.333333 0 0 0 53.333333-53.333333V448z" fill="#707070" p-id="2881"></path></svg></div>`);
+        $("."+holder).append(`<div title="编辑提问" style="width:35px;margin:0 5 0 5;"><svg t="1677572686028" class=a${id} id="edit_data" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2880" width="20" height="20"><path d="M862.709333 116.042667a32 32 0 1 1 45.248 45.248L455.445333 613.813333a32 32 0 1 1-45.258666-45.258666L862.709333 116.053333zM853.333333 448a32 32 0 0 1 64 0v352c0 64.8-52.533333 117.333333-117.333333 117.333333H224c-64.8 0-117.333333-52.533333-117.333333-117.333333V224c0-64.8 52.533333-117.333333 117.333333-117.333333h341.333333a32 32 0 0 1 0 64H224a53.333333 53.333333 0 0 0-53.333333 53.333333v576a53.333333 53.333333 0 0 0 53.333333 53.333333h576a53.333333 53.333333 0 0 0 53.333333-53.333333V448z" fill="#707070" p-id="2881"></path></svg></div>`);
     }
     todown_now();
 }
